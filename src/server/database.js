0e0d2ed24fda14be4100c43c694718cfcd93c7bb
carpe <carpeas@outlook.es>
2024-06-12 20:27:37 +0200

    seguridad credenciales bd

--- src/server/database.js
+++ src/server/database.js
@@ -1,11 +1,11 @@
 const mariadb = require('mariadb');
+require('dotenv').config();
 
 const pool = mariadb.createPool({
-  host: 'localhost',
-  user: 'admin',     
-  password: 'pass16643',
-  database: 'app_db',
-  connectionLimit: 5
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,     
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
